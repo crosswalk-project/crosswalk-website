c3c66cf7676ba1835f8ee910ab91d3fe4c544062
James Ketrenos <james.p.ketrenos@intel.com>
2013-09-02 11:51:11 -0700

    Removed menus.js.php when going live
    Signed-off-by: James Ketrenos <james.p.ketrenos@intel.com>

--- mklive.sh
+++ mklive.sh
@@ -14,6 +14,9 @@ function check_unstaged () {
 	}
 }
 
+#
+# Verify this tree is on the 'live' branch
+#
 git branch | grep -q '\* live' || {
 	echo ""
 	echo "mklive.sh can only be run in the 'live' branch."
@@ -22,20 +25,34 @@ git branch | grep -q '\* live' || {
 	exit
 }
 
+#
+# Ensure no unstaged changes in wiki/ or /
+#
 check_unstaged
 cd wiki
 check_unstaged
 cd ..
 
+#
+# Nuke all dynamic content and regenerate it
+#
 ./cleanup.sh
 ./generate.sh
 
+#
+# Turn off the PHP script override for the root
+# directory.
+#
 cat << EOF > .htaccess
 RewriteEngine on
 RewriteCond %{REQUEST_FILENAME} -f
 RewriteRule ^(\..*) %{REQUEST_URI}$1 [R=404]
 EOF
 
+#
+# Modify .gitignore to track/manage the generated content
+# from under the wiki/ path
+#
 sed -i -e 's:^wiki/\*\.html:#wiki/*.html:' \
        -e 's:^wiki/documentation:#wiki/documentation:' \
        -e 's:^wiki/contribute:#wiki/contribute:' .gitignore
@@ -43,17 +60,27 @@ for i in xwalk.css markdown.css menus.js; do
 	sed -i -e s:^${i}:#${i}: .gitignore
 done
 
+#
+# Adding generated Wiki content to the Live site
+#
 find wiki -name '*html' -exec git add {} \;
 git add xwalk.css
 git add markdown.css
 git add menus.js
 
+#
+# Remove PHP generating scripts from Live site
+#
 for file in xwalk markdown; do
 	for extension in msg css.php scss css.map; do
 		[ -e ${file}.${extension} ] && rm ${file}.${extension}
 	done
 done
+[ -e menus.js.php ] && rm menus.js.php
 
+#
+# Remove all Wiki markdown content from Live site
+#
 find wiki -name '*.md' -exec rm {} \;
 find wiki -name '*.mediawiki' -exec rm {} \;
 find wiki -name '*.org' -exec rm {} \;
