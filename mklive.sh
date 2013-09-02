f9796ac79fe1fc3fc4040b7452d0faab12fc7e0e
James Ketrenos <james.p.ketrenos@intel.com>
2013-09-02 10:23:19 -0700

    mklive.sh can only be run on live branch
    Signed-off-by: James Ketrenos <james.p.ketrenos@intel.com>

--- mklive.sh
+++ mklive.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+git branch | grep '\* live' || {
+	echo ""
+	echo "mklive.sh can only be run in the 'live' branch."
+	echo "$ git checkout live"
+	echo ""
+	exit
+}
 git status >/dev/null 2>&1 && {
 	echo ""
 	echo "Untracked local changes. Can't go live."
