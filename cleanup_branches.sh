db7fe84c535fe2ede4bf18bfd8c4c2879e13550a
James Ketrenos <james.p.ketrenos@intel.com>
2013-09-26 16:56:09 -0700

    Fixed cleanup_branches to remove local branches too
    Signed-off-by: James Ketrenos <james.p.ketrenos@intel.com>

--- cleanup_branches.sh
+++ cleanup_branches.sh
@@ -2,16 +2,16 @@
 today=$(date +%Y%m%d)
 last_week=$((today - 7))
 work_done=0
-git branch | grep -v '\*.*' | while read branch; do git branch -D ; done
-git branch -r | grep ".*/live" | while read branch; do
-	branch_date=${branch/*origin\/live-}
+git branch -a | grep ".*live" | while read branch; do
+	branch_date=${branch/*live-}
 	branch_date=${branch_date//.*}
 	if (( branch_date <= last_week )); then
 		work_done=1
-		git branch -d -r ${branch}
+		git push origin :${branch/*origin\/}
+		git branch -D ${branch/*origin\/}
 	fi
 done
-(( ${work_done} )) && {
+(( work_done )) && {
 cat << EOF
 Branches have been purged. Commands to run:
 
