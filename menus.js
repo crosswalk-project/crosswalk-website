d95ec346f894dfff6c16dd63c3728e7c1da4612c
Elliot Smith <elliot.smith@intel.com>
2014-01-30 16:05:05 +0000

    Automatic static version commit for live-20140130
    Signed-off-by: Elliot Smith <elliot.smith@intel.com>

---
+++ menus.js
@@ -0,0 +1,135 @@
+var menus = [
+    {
+        "menu": "documentation",
+        "items": [
+            {
+                "wiki": "00-Getting_Started",
+                "name": "Getting Started",
+                "file": "Getting_Started",
+                "subpages": [
+                    {
+                        "wiki": "05-Installing_Crosswalk",
+                        "name": "Installing Crosswalk",
+                        "file": "Installing_Crosswalk"
+                    },
+                    {
+                        "wiki": "10-Building_an_Application",
+                        "name": "Building an Application",
+                        "file": "Building_an_Application"
+                    },
+                    {
+                        "wiki": "30-Running_an_Application",
+                        "name": "Running an Application",
+                        "file": "Running_an_Application"
+                    }
+                ]
+            },
+            {
+                "wiki": "01-Downloads",
+                "name": "Downloads",
+                "file": "Downloads",
+                "subpages": [
+                    {
+                        "wiki": "Beta",
+                        "name": "Beta",
+                        "file": "Beta"
+                    },
+                    {
+                        "wiki": "Canary",
+                        "name": "Canary",
+                        "file": "Canary"
+                    }
+                ]
+            },
+            {
+                "wiki": "70-Samples",
+                "name": "Samples",
+                "file": "Samples",
+                "subpages": [
+                    {
+                        "wiki": "Hello-world",
+                        "name": "Hello world",
+                        "file": "Hello-world"
+                    },
+                    {
+                        "wiki": "Tizen",
+                        "name": "Tizen",
+                        "file": "Tizen"
+                    },
+                    {
+                        "wiki": "WebGL",
+                        "name": "WebGL",
+                        "file": "WebGL"
+                    }
+                ]
+            },
+            {
+                "wiki": "80-APIs",
+                "name": "APIs",
+                "file": "APIs"
+            },
+            {
+                "wiki": "90-Community",
+                "name": "Community",
+                "file": "Community"
+            },
+            {
+                "wiki": "Developing-on-Windows",
+                "name": "Developing on Windows",
+                "file": "Developing-on-Windows"
+            }
+        ]
+    },
+    {
+        "menu": "contribute",
+        "items": [
+            {
+                "wiki": "00-Overview",
+                "name": "Overview",
+                "file": "Overview"
+            },
+            {
+                "wiki": "99-Channels-Viewer",
+                "name": "Channels Viewer",
+                "file": "Channels-Viewer"
+            },
+            {
+                "wiki": "Building_Crosswalk",
+                "name": "Building Crosswalk",
+                "file": "Building_Crosswalk"
+            },
+            {
+                "wiki": "Contributing-Code",
+                "name": "Contributing Code",
+                "file": "Contributing-Code"
+            },
+            {
+                "wiki": "Rebasing-Strategy",
+                "name": "Rebasing Strategy",
+                "file": "Rebasing-Strategy"
+            },
+            {
+                "wiki": "Reviewer_Policy",
+                "name": "Reviewer Policy",
+                "file": "Reviewer_Policy"
+            }
+        ]
+    },
+    {
+        "menu": "wiki",
+        "items": [
+            {
+                "name": "Home",
+                "file": "Home"
+            },
+            {
+                "name": "Pages",
+                "file": "Pages"
+            },
+            {
+                "name": "History",
+                "file": "History"
+            }
+        ]
+    }
+];
