69ec020f4f32093007b36b147851849963609344
Elliot Smith <elliot.smith@intel.com>
2014-02-11 14:45:37 +0000

    Downloads page now uses github API

--- documentation/01-Downloads.php
+++ documentation/01-Downloads.php
@@ -10,7 +10,7 @@
     <a href="#documentation/getting_started">Getting started</a> pages.</p>
 
     <p>For more information about the release channels, see
-    <a href="#Release-channels">the <em>Release channels</em> section</a></p>.
+    <a href="#Release-channels">the <em>Release channels</em> section</a>.</p>
 
     <table class="downloads-table">
 
@@ -23,50 +23,78 @@
 
     <tr>
     <th>Android (x86)</th>
-    <td><a href="https://download.01.org/crosswalk/releases/android-x86/stable/crosswalk-${XWALK-STABLE-ANDROID-X86}-x86.zip">${XWALK-STABLE-ANDROID-X86}</a></td>
-    <td><a href="https://download.01.org/crosswalk/releases/android-x86/beta/crosswalk-${XWALK-BETA-ANDROID-X86}-x86.zip">${XWALK-BETA-ANDROID-X86}</a></td>
-    <td><a href="https://download.01.org/crosswalk/releases/android-x86/canary/">downloads</a></td>
+    <td data-role="download-cell" data-loading="true">
+    <a data-role="download-link" data-channel="stable" data-os="android-x86"></a>
+    </td>
+    <td data-role="download-cell" data-loading="true">
+    <a data-role="download-link" data-channel="beta" data-os="android-x86"></a>
+    </td>
+    <td data-role="download-cell" data-loading="true">
+    <a data-role="download-link" data-channel="canary" data-os="android-x86"></a>
+    </td>
     </tr>
 
     <tr>
     <th>Android (ARM)</th>
     <td>-</td>
     <td>-</td>
-    <td><a href="https://download.01.org/crosswalk/releases/android-arm/canary/">downloads</a></td>
+    <td data-role="download-cell" data-loading="true">
+    <a data-role="download-link" data-channel="canary" data-os="android-arm"></a>
+    </td>
     </tr>
 
     <tr>
     <th>Tizen 2.x Mobile (x86)</th>
-    <td><a href="https://download.01.org/crosswalk/releases/tizen-mobile/stable/crosswalk-${XWALK-STABLE-TIZEN-X86}-0.i586.rpm">${XWALK-STABLE-TIZEN-X86}</a></td>
-    <td><a href="https://download.01.org/crosswalk/releases/tizen-mobile/beta/crosswalk-${XWALK-BETA-TIZEN-X86}-0.i586.rpm">${XWALK-BETA-TIZEN-X86}</a></td>
-    <td><a href="https://download.01.org/crosswalk/releases/tizen-mobile/canary/">downloads</a></td>
+    <td data-role="download-cell" data-loading="true">
+    <a data-role="download-link" data-channel="stable" data-os="tizen-mobile"></a>
+    </td>
+    <td data-role="download-cell" data-loading="true">
+    <a data-role="download-link" data-channel="beta" data-os="tizen-mobile"></a>
+    </td>
+    <td data-role="download-cell" data-loading="true">
+    <a data-role="download-link" data-channel="canary" data-os="tizen-mobile"></a>
+    </td>
     </tr>
 
     <tr>
     <th>Tizen 2.x Emulator (x86)</th>
-    <td><a href="https://download.01.org/crosswalk/releases/tizen-mobile/stable/crosswalk-emulator-support-${XWALK-STABLE-TIZEN-X86}-0.i586.rpm">${XWALK-STABLE-TIZEN-X86}</a></td>
-    <td><a href="https://download.01.org/crosswalk/releases/tizen-mobile/beta/crosswalk-emulator-support-${XWALK-BETA-TIZEN-X86}-0.i586.rpm">${XWALK-BETA-TIZEN-X86}</a></td>
-    <td><a href="https://download.01.org/crosswalk/releases/tizen-mobile/canary/">downloads</a></td>
+    <td data-role="download-cell" data-loading="true">
+    <a data-role="download-link" data-channel="stable" data-os="tizen-emulator"></a>
+    </td>
+    <td data-role="download-cell" data-loading="true">
+    <a data-role="download-link" data-channel="beta" data-os="tizen-emulator"></a>
+    </td>
+    <td data-role="download-cell" data-loading="true">
+    <a data-role="download-link" data-channel="canary" data-os="tizen-emulator"></a>
+    </td>
     </tr>
 
     <tr>
     <th>Tizen 3.0 IVI (x86)</th>
     <td>-</td>
     <td>-</td>
-    <td><a href="https://download.01.org/crosswalk/releases/tizen-ivi/canary/">downloads</a></td>
+    <td data-role="download-cell" data-loading="true">
+    <a data-role="download-link" data-channel="canary" data-os="tizen-ivi"></a>
+    </td>
     </tr>
 
     <tr class="downloads-row release-notes-row">
     <th class="release-notes-heading">Release notes</th>
-    <td><a href="#wiki/Crosswalk-${XWALK-STABLE-ANDROID-X86-MAJOR}-release-notes">Crosswalk-${XWALK-STABLE-ANDROID-X86-MAJOR}</a></td>
-    <td><a href="#wiki/Crosswalk-${XWALK-BETA-ANDROID-X86-MAJOR}-release-notes">Crosswalk-${XWALK-BETA-ANDROID-X86-MAJOR}</a></td>
+    <td data-role="download-cell" data-loading="true">
+    <a data-role="release-notes-link" data-channel="stable"></a>
+    </td>
+    <td data-role="download-cell" data-loading="true">
+    <a data-role="release-notes-link" data-channel="beta"></a>
+    </td>
     <td>*</td>
     </tr>
 
     </table>
 
-    <p>\* - note that release notes are only produced for stable and beta
+    <p>* - note that release notes are only produced for stable and beta
     versions.</p>
+
+    <p><a href="https://download.01.org/crosswalk/releases/">More downloads...</a></p>
 
     <h2>Release channels</h2>
 
@@ -118,7 +146,87 @@
     version numbers page</a> describes how Crosswalk versions are assigned.</p>
 
     <script>
-    alert('it is me!!!!');
+    // populate the download links and release notes links in the table
+    var sel = 'td[data-role="download-cell"]'
+    var dlCells = document.querySelectorAll(sel);
+
+    // these are release note links; to avoid making unnecessary calls
+    // to github, we populate these when we retrieve the versions for
+    // the stable and beta channels
+    sel = 'a[data-role="release-notes-link"]';
+    var releaseNotesLinks = document.querySelectorAll(sel);
+
+    // make a callback to populate a download link once the version has
+    // been retrieved; note that (for the sake of efficiency) we also
+    // check through the release note links and populate those at the
+    // same time, if the channel of the release note link matches the
+    // channel download link we are populating (to avoid an extra fetch
+    // from the github API)
+    var makeCb = function (cell, link, channel) {
+      var os = link.getAttribute('data-os');
+
+      return function (err, response) {
+          // get the URL for the download
+          var url = getXwalkDownloadUrl(os, channel, response.version);
+
+          // remove the spinner
+          cell.setAttribute('data-loading', 'false');
+
+          // set the URL and text for the download link
+          link.setAttribute('href', url);
+          link.innerText = response.version;
+
+          // fill in any of the release notes links which apply
+          // for the downloaded version
+          for (var j = 0; j < releaseNotesLinks.length; j += 1) {
+              var rnLink = releaseNotesLinks.item(j);
+
+              if (rnLink.getAttribute('data-channel') === channel) {
+                  var majorVersion = getXwalkMajorVersion(response.version);
+                  rnLink.setAttribute('href', getReleaseNotesUrl(majorVersion));
+                  rnLink.innerText = 'Crosswalk-' + majorVersion;
+
+                  // if the parent node has data-loading="true", remove it
+                  // to turn off the spinner
+                  if (rnLink.parentNode.getAttribute('data-loading') === 'true') {
+                      rnLink.parentNode.setAttribute('data-loading', 'false');
+                  }
+              }
+          }
+      }
+    };
+
+    // get the current branches
+    asyncJsonGet('./github.php', function (err, branches) {
+        // populate download links
+        for (var j = 0; j < dlCells.length; j += 1) {
+            var cell = dlCells.item(j);
+            var link = cell.querySelector('a[data-role="download-link"]');
+
+            // if this cell has no download link, ignore it
+            // (it's a release notes cell)
+            if (!link) {
+                continue;
+            }
+
+            var channel = link.getAttribute('data-channel');
+
+            // get the branch for the channel
+            var branch;
+            for (var i = 0; i < branches.length; i += 1) {
+                if (branches[i].channel === channel) {
+                    branch = branches[i].branch;
+                    break;
+                }
+            }
+
+            // get the version for the branch; we pass a callback
+            // to the async JSON download, which fills the link text
+            // and URL when the JSON is returned
+            var path = './github.php?fetch=version&branch=' + branch;
+            asyncJsonGet(path, makeCb(cell, link, channel));
+        }
+    });
     </script>
   </body>
 </html>
--- utils.js
+++ utils.js
@@ -26,20 +26,24 @@ function asyncJsonGet(path, cb) {
     xhr.send();
 }
 
-// get the download URL for a Crosswalk OS/channel/version;
+// get the download URL for a Crosswalk OS/channel/version
+// (see for how they're organised);
 // channel: 'stable', 'beta', 'canary'
 // OS: 'android-x86', 'android-arm', 'tizen-mobile', 'tizen-ivi',
 // 'tizen-emulator'
 // version: e.g. "4.32.25.1"
 function getXwalkDownloadUrl(OS, channel, version) {
-    var download_url = 'https://download.01.org/crosswalk/releases/' +
-                       OS + "/" + channel + "/crosswalk-";
+    var file_prefix = "crosswalk-";
 
+    // tizen emulator downloads are in the same directory as the
+    // tizen-mobile ones...
     if (OS === "tizen-emulator") {
-        download_url += "emulator-support-";
+        OS = "tizen-mobile";
+        file_prefix += "emulator-support-";
     }
 
-    download_url += version;
+    var download_url = 'https://download.01.org/crosswalk/releases/' +
+                       OS + "/" + channel + "/" + file_prefix + version;
 
     if (OS === "android-x86") {
         download_url += "-x86.zip";
@@ -47,9 +51,34 @@ function getXwalkDownloadUrl(OS, channel, version) {
     else if (OS === "android-arm") {
         download_url += "-arm.zip";
     }
+    else if (OS === "tizen-ivi") {
+        download_url += "-0.i686.rpm";
+    }
     else {
         download_url += "-0.i586.rpm";
     }
 
     return download_url;
+}
+
+// version is a full Crosswalk version number, e.g. "4.23.4.2";
+// we just take the digits up to the first '.'
+function getXwalkMajorVersion(version) {
+    var matches = /^(\d+)?\./.exec(version);
+
+    if (matches.length < 2) {
+        majorVersion = '0';
+    }
+    else {
+        majorVersion = matches[1];
+    }
+
+    return majorVersion;
+}
+
+// return a URL for the release notes for majorVersion in the wiki;
+// NB this assumes that release notes are given a consistent name
+// "Crosswalk N release notes"
+function getReleaseNotesUrl(majorVersion) {
+    return "#wiki/Crosswalk-" + majorVersion + "-release-notes";
 }
