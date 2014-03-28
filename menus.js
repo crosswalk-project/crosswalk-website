116b429b0ff05ca8ea15ce6847141bf3e19f6bf5
Elliot Smith <elliot.smith@intel.com>
2014-03-28 15:27:25 +0000

    Automatic static version commit for live-20140328.6
    Signed-off-by: Elliot Smith <elliot.smith@intel.com>

---
+++ menus.js
@@ -0,0 +1,167 @@
+var menus = [
+    {
+        "menu": "documentation",
+        "items": [
+            {
+                "wiki": "00-About",
+                "name": "About",
+                "file": "About",
+                "subpages": [
+                    {
+                        "wiki": "10-Faq",
+                        "name": "Faq",
+                        "file": "Faq"
+                    }
+                ]
+            },
+            {
+                "wiki": "01-Getting_Started",
+                "name": "Getting Started",
+                "file": "Getting_Started",
+                "subpages": [
+                    {
+                        "wiki": "10-Host_setup",
+                        "name": "Host setup",
+                        "file": "Host_setup"
+                    },
+                    {
+                        "wiki": "12-Android_target_setup",
+                        "name": "Android target setup",
+                        "file": "Android_target_setup"
+                    },
+                    {
+                        "wiki": "15-Tizen_target_setup",
+                        "name": "Tizen target setup",
+                        "file": "Tizen_target_setup"
+                    },
+                    {
+                        "wiki": "20-Build_an_application",
+                        "name": "Build an application",
+                        "file": "Build_an_application"
+                    },
+                    {
+                        "wiki": "30-Run_on_Android",
+                        "name": "Run on Android",
+                        "file": "Run_on_Android"
+                    },
+                    {
+                        "wiki": "40-Run_on_Tizen",
+                        "name": "Run on Tizen",
+                        "file": "Run_on_Tizen"
+                    }
+                ]
+            },
+            {
+                "wiki": "10-Tizen_IVI_extensions",
+                "name": "Tizen IVI extensions",
+                "file": "Tizen_IVI_extensions",
+                "subpages": [
+                    {
+                        "wiki": "10-Host_and_target_setup",
+                        "name": "Host and target setup",
+                        "file": "Host_and_target_setup"
+                    },
+                    {
+                        "wiki": "20-Build_application_and_extension",
+                        "name": "Build application and extension",
+                        "file": "Build_application_and_extension"
+                    },
+                    {
+                        "wiki": "30-Run_on_Tizen_emulator",
+                        "name": "Run on Tizen emulator",
+                        "file": "Run_on_Tizen_emulator"
+                    }
+                ]
+            },
+            {
+                "wiki": "20-Downloads",
+                "name": "Downloads",
+                "file": "Downloads"
+            },
+            {
+                "wiki": "70-Samples",
+                "name": "Samples",
+                "file": "Samples",
+                "subpages": [
+                    {
+                        "wiki": "Hello-world",
+                        "name": "Hello world",
+                        "file": "Hello-world"
+                    },
+                    {
+                        "wiki": "Tizen",
+                        "name": "Tizen",
+                        "file": "Tizen"
+                    },
+                    {
+                        "wiki": "WebGL",
+                        "name": "WebGL",
+                        "file": "WebGL"
+                    }
+                ]
+            },
+            {
+                "wiki": "80-APIs",
+                "name": "APIs",
+                "file": "APIs"
+            },
+            {
+                "wiki": "90-Community",
+                "name": "Community",
+                "file": "Community"
+            }
+        ]
+    },
+    {
+        "menu": "contribute",
+        "items": [
+            {
+                "wiki": "00-Overview",
+                "name": "Overview",
+                "file": "Overview"
+            },
+            {
+                "wiki": "99-Channels-Viewer",
+                "name": "Channels Viewer",
+                "file": "Channels-Viewer"
+            },
+            {
+                "wiki": "Building_Crosswalk",
+                "name": "Building Crosswalk",
+                "file": "Building_Crosswalk"
+            },
+            {
+                "wiki": "Contributing-Code",
+                "name": "Contributing Code",
+                "file": "Contributing-Code"
+            },
+            {
+                "wiki": "Downstream-Chromium",
+                "name": "Downstream Chromium",
+                "file": "Downstream-Chromium"
+            },
+            {
+                "wiki": "Reviewer_Policy",
+                "name": "Reviewer Policy",
+                "file": "Reviewer_Policy"
+            }
+        ]
+    },
+    {
+        "menu": "wiki",
+        "items": [
+            {
+                "name": "Home",
+                "file": "Home"
+            },
+            {
+                "name": "Pages",
+                "file": "Pages"
+            },
+            {
+                "name": "History",
+                "file": "History"
+            }
+        ]
+    }
+];
