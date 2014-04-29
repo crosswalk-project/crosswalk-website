5cf6689eef870de8c361f935a316322993e3fc9a
Elliot Smith <elliot.smith@intel.com>
2014-04-29 09:50:30 +0100

    Automatic static version commit for live-20140429
    Signed-off-by: Elliot Smith <elliot.smith@intel.com>

--- contribute/Downstream-Chromium.md
+++
@@ -1,3 +0,0 @@
-Please visit the wiki page describing the rebasing strategy we have in place :
-
-[Crosswalk and Downstream Chromium](#wiki/Downstream-Chromium).
---
+++ menus.js
@@ -0,0 +1,187 @@
+var menus = [
+    {
+        "menu": "documentation",
+        "items": [
+            {
+                "wiki": "00-About",
+                "name": "About",
+                "file": "About",
+                "subpages": [
+                    {
+                        "wiki": "10-Faq",
+                        "name": "Faq",
+                        "file": "Faq"
+                    }
+                ]
+            },
+            {
+                "wiki": "01-Getting_Started",
+                "name": "Getting Started",
+                "file": "Getting_Started",
+                "subpages": [
+                    {
+                        "wiki": "10-Windows_host_setup",
+                        "name": "Windows host setup",
+                        "file": "Windows_host_setup"
+                    },
+                    {
+                        "wiki": "11-Linux_host_setup",
+                        "name": "Linux host setup",
+                        "file": "Linux_host_setup"
+                    },
+                    {
+                        "wiki": "12-Android_target_setup",
+                        "name": "Android target setup",
+                        "file": "Android_target_setup"
+                    },
+                    {
+                        "wiki": "15-Tizen_target_setup",
+                        "name": "Tizen target setup",
+                        "file": "Tizen_target_setup"
+                    },
+                    {
+                        "wiki": "20-Build_an_application",
+                        "name": "Build an application",
+                        "file": "Build_an_application"
+                    },
+                    {
+                        "wiki": "30-Run_on_Android",
+                        "name": "Run on Android",
+                        "file": "Run_on_Android"
+                    },
+                    {
+                        "wiki": "40-Run_on_Tizen",
+                        "name": "Run on Tizen",
+                        "file": "Run_on_Tizen"
+                    },
+                    {
+                        "wiki": "50-Deploy_to_Android_store",
+                        "name": "Deploy to Android store",
+                        "file": "Deploy_to_Android_store"
+                    }
+                ]
+            },
+            {
+                "wiki": "10-Tizen_IVI_extensions",
+                "name": "Tizen IVI extensions",
+                "file": "Tizen_IVI_extensions",
+                "subpages": [
+                    {
+                        "wiki": "10-Host_and_target_setup",
+                        "name": "Host and target setup",
+                        "file": "Host_and_target_setup"
+                    },
+                    {
+                        "wiki": "15-Write_an_extension",
+                        "name": "Write an extension",
+                        "file": "Write_an_extension"
+                    },
+                    {
+                        "wiki": "20-Write_a_web_application",
+                        "name": "Write a web application",
+                        "file": "Write_a_web_application"
+                    },
+                    {
+                        "wiki": "30-Run_on_Tizen_VM",
+                        "name": "Run on Tizen VM",
+                        "file": "Run_on_Tizen_VM"
+                    }
+                ]
+            },
+            {
+                "wiki": "20-Downloads",
+                "name": "Downloads",
+                "file": "Downloads"
+            },
+            {
+                "wiki": "70-Samples",
+                "name": "Samples",
+                "file": "Samples",
+                "subpages": [
+                    {
+                        "wiki": "Hello-world",
+                        "name": "Hello world",
+                        "file": "Hello-world"
+                    },
+                    {
+                        "wiki": "SIMD",
+                        "name": "SIMD",
+                        "file": "SIMD"
+                    },
+                    {
+                        "wiki": "Tizen",
+                        "name": "Tizen",
+                        "file": "Tizen"
+                    },
+                    {
+                        "wiki": "WebGL",
+                        "name": "WebGL",
+                        "file": "WebGL"
+                    }
+                ]
+            },
+            {
+                "wiki": "80-APIs",
+                "name": "APIs",
+                "file": "APIs"
+            },
+            {
+                "wiki": "90-Community",
+                "name": "Community",
+                "file": "Community"
+            }
+        ]
+    },
+    {
+        "menu": "contribute",
+        "items": [
+            {
+                "wiki": "00-Overview",
+                "name": "Overview",
+                "file": "Overview"
+            },
+            {
+                "wiki": "99-Channels-Viewer",
+                "name": "Channels Viewer",
+                "file": "Channels-Viewer"
+            },
+            {
+                "wiki": "Building_Crosswalk",
+                "name": "Building Crosswalk",
+                "file": "Building_Crosswalk"
+            },
+            {
+                "wiki": "Contributing-Code",
+                "name": "Contributing Code",
+                "file": "Contributing-Code"
+            },
+            {
+                "wiki": "Downstream-Chromium",
+                "name": "Downstream Chromium",
+                "file": "Downstream-Chromium"
+            },
+            {
+                "wiki": "Reviewer_Policy",
+                "name": "Reviewer Policy",
+                "file": "Reviewer_Policy"
+            }
+        ]
+    },
+    {
+        "menu": "wiki",
+        "items": [
+            {
+                "name": "Home",
+                "file": "Home"
+            },
+            {
+                "name": "Pages",
+                "file": "Pages"
+            },
+            {
+                "name": "History",
+                "file": "History"
+            }
+        ]
+    }
+];
