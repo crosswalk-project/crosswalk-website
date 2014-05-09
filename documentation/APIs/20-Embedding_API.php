877804a16833ad698c409fed35d027b8cce4a6fb
Elliot Smith <elliot.smith@intel.com>
2014-05-09 08:37:44 +0100

    Make new APIs directory

---
+++ documentation/APIs/20-Embedding_API.php
@@ -0,0 +1,5 @@
+<?php
+$location = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['CONTEXT_PREFIX'] .
+            '/apis/embeddingapidocs/reference/org/xwalk/core/package-summary.html';
+?>
+<p><a href="<?php echo $location ?>" target="_blank">Javadocs for the API</a></p>
