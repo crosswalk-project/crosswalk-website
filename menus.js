0d43a9bc6d7851c53929ea4ad92c15f102093e16
Elliot Smith <elliot.smith@intel.com>
2014-07-18 10:35:01 +0100

    Automatic static version commit for live-20140718
    Signed-off-by: Elliot Smith <elliot.smith@intel.com>

---
+++ menus.js
@@ -0,0 +1,330 @@
+var menus = [
+    {
+        "menu": "documentation",
+        "items": [
+            {
+                "wiki": "00-About",
+                "name": "About",
+                "file": "About",
+                "subpages": [
+                    {
+                        "wiki": "10-Faq",
+                        "name": "Faq",
+                        "file": "Faq"
+                    }
+                ]
+            },
+            {
+                "wiki": "01-Getting_Started",
+                "name": "Getting Started",
+                "file": "Getting_Started",
+                "subpages": [
+                    {
+                        "wiki": "10-Windows_host_setup",
+                        "name": "Windows host setup",
+                        "file": "Windows_host_setup"
+                    },
+                    {
+                        "wiki": "11-Linux_host_setup",
+                        "name": "Linux host setup",
+                        "file": "Linux_host_setup"
+                    },
+                    {
+                        "wiki": "12-Android_target_setup",
+                        "name": "Android target setup",
+                        "file": "Android_target_setup"
+                    },
+                    {
+                        "wiki": "15-Tizen_target_setup",
+                        "name": "Tizen target setup",
+                        "file": "Tizen_target_setup"
+                    },
+                    {
+                        "wiki": "20-Build_an_application",
+                        "name": "Build an application",
+                        "file": "Build_an_application"
+                    },
+                    {
+                        "wiki": "30-Run_on_Android",
+                        "name": "Run on Android",
+                        "file": "Run_on_Android"
+                    },
+                    {
+                        "wiki": "40-Run_on_Tizen",
+                        "name": "Run on Tizen",
+                        "file": "Run_on_Tizen"
+                    },
+                    {
+                        "wiki": "50-Deploy_to_Android_store",
+                        "name": "Deploy to Android store",
+                        "file": "Deploy_to_Android_store"
+                    }
+                ]
+            },
+            {
+                "wiki": "05-Manifest",
+                "name": "Manifest",
+                "file": "Manifest",
+                "subpages": [
+                    {
+                        "wiki": "00-Using_the_manifest",
+                        "name": "Using the manifest",
+                        "file": "Using_the_manifest"
+                    },
+                    {
+                        "wiki": "01-Entry_points",
+                        "name": "Entry points",
+                        "file": "Entry_points"
+                    },
+                    {
+                        "wiki": "content_security_policy",
+                        "name": "content security policy",
+                        "file": "content_security_policy"
+                    },
+                    {
+                        "wiki": "display",
+                        "name": "display",
+                        "file": "display"
+                    },
+                    {
+                        "wiki": "icons",
+                        "name": "icons",
+                        "file": "icons"
+                    },
+                    {
+                        "wiki": "launch_screen",
+                        "name": "launch screen",
+                        "file": "launch_screen"
+                    },
+                    {
+                        "wiki": "permissions",
+                        "name": "permissions",
+                        "file": "permissions"
+                    },
+                    {
+                        "wiki": "xwalk_hosts",
+                        "name": "xwalk hosts",
+                        "file": "xwalk_hosts"
+                    }
+                ]
+            },
+            {
+                "wiki": "07-Android_extensions",
+                "name": "Android extensions",
+                "file": "Android_extensions",
+                "subpages": [
+                    {
+                        "wiki": "01-Host_and_target_setup",
+                        "name": "Host and target setup",
+                        "file": "Host_and_target_setup"
+                    },
+                    {
+                        "wiki": "11-Write_an_extension",
+                        "name": "Write an extension",
+                        "file": "Write_an_extension"
+                    },
+                    {
+                        "wiki": "21-Write_a_web_application",
+                        "name": "Write a web application",
+                        "file": "Write_a_web_application"
+                    },
+                    {
+                        "wiki": "31-Run_on_Android",
+                        "name": "Run on Android",
+                        "file": "Run_on_Android"
+                    }
+                ]
+            },
+            {
+                "wiki": "10-Tizen_IVI_extensions",
+                "name": "Tizen IVI extensions",
+                "file": "Tizen_IVI_extensions",
+                "subpages": [
+                    {
+                        "wiki": "10-Host_and_target_setup",
+                        "name": "Host and target setup",
+                        "file": "Host_and_target_setup"
+                    },
+                    {
+                        "wiki": "15-Write_an_extension_in_C",
+                        "name": "Write an extension in C",
+                        "file": "Write_an_extension_in_C"
+                    },
+                    {
+                        "wiki": "17-Write_an_extension_in_C++",
+                        "name": "Write an extension in C++",
+                        "file": "Write_an_extension_in_C++"
+                    },
+                    {
+                        "wiki": "18-JSON_based_C++_extension",
+                        "name": "JSON based C++ extension",
+                        "file": "JSON_based_C++_extension"
+                    },
+                    {
+                        "wiki": "20-Write_a_web_application",
+                        "name": "Write a web application",
+                        "file": "Write_a_web_application"
+                    },
+                    {
+                        "wiki": "30-Run_on_Tizen_VM",
+                        "name": "Run on Tizen VM",
+                        "file": "Run_on_Tizen_VM"
+                    }
+                ]
+            },
+            {
+                "wiki": "15-Cordova",
+                "name": "Cordova",
+                "file": "Cordova",
+                "subpages": [
+                    {
+                        "wiki": "10-Develop_an_application",
+                        "name": "Develop an application",
+                        "file": "Develop_an_application"
+                    },
+                    {
+                        "wiki": "20-Add_plugins",
+                        "name": "Add plugins",
+                        "file": "Add_plugins"
+                    },
+                    {
+                        "wiki": "30-Migrate_an_application",
+                        "name": "Migrate an application",
+                        "file": "Migrate_an_application"
+                    }
+                ]
+            },
+            {
+                "wiki": "16-Embedding_Crosswalk",
+                "name": "Embedding Crosswalk",
+                "file": "Embedding_Crosswalk"
+            },
+            {
+                "wiki": "17-WebRTC",
+                "name": "WebRTC",
+                "file": "WebRTC"
+            },
+            {
+                "wiki": "20-Downloads",
+                "name": "Downloads",
+                "file": "Downloads"
+            },
+            {
+                "wiki": "70-Samples",
+                "name": "Samples",
+                "file": "Samples",
+                "subpages": [
+                    {
+                        "wiki": "Hello-world",
+                        "name": "Hello world",
+                        "file": "Hello-world"
+                    },
+                    {
+                        "wiki": "SIMD",
+                        "name": "SIMD",
+                        "file": "SIMD"
+                    },
+                    {
+                        "wiki": "Tizen",
+                        "name": "Tizen",
+                        "file": "Tizen"
+                    },
+                    {
+                        "wiki": "WebGL",
+                        "name": "WebGL",
+                        "file": "WebGL"
+                    }
+                ]
+            },
+            {
+                "wiki": "80-APIs",
+                "name": "APIs",
+                "file": "APIs",
+                "subpages": [
+                    {
+                        "wiki": "10-Web_APIs",
+                        "name": "Web APIs",
+                        "file": "Web_APIs"
+                    },
+                    {
+                        "wiki": "20-Embedding_API",
+                        "name": "Embedding API",
+                        "file": "Embedding_API"
+                    }
+                ]
+            },
+            {
+                "wiki": "90-Community",
+                "name": "Community",
+                "file": "Community"
+            },
+            {
+                "wiki": "95-Report_bugs",
+                "name": "Report bugs",
+                "file": "Report_bugs"
+            }
+        ]
+    },
+    {
+        "menu": "contribute",
+        "items": [
+            {
+                "wiki": "00-Overview",
+                "name": "Overview",
+                "file": "Overview"
+            },
+            {
+                "wiki": "01-Channels_Viewer",
+                "name": "Channels Viewer",
+                "file": "Channels_Viewer"
+            },
+            {
+                "wiki": "05-Building_Crosswalk",
+                "name": "Building Crosswalk",
+                "file": "Building_Crosswalk"
+            },
+            {
+                "wiki": "10-Contributing-Code",
+                "name": "Contributing Code",
+                "file": "Contributing-Code"
+            },
+            {
+                "wiki": "11-Coding_style",
+                "name": "Coding style",
+                "file": "Coding_style"
+            },
+            {
+                "wiki": "15-Downstream-Chromium",
+                "name": "Downstream Chromium",
+                "file": "Downstream-Chromium"
+            },
+            {
+                "wiki": "20-Reviewer_Policy",
+                "name": "Reviewer Policy",
+                "file": "Reviewer_Policy"
+            },
+            {
+                "wiki": "99-Report_bugs",
+                "name": "Report bugs",
+                "file": "Report_bugs"
+            }
+        ]
+    },
+    {
+        "menu": "wiki",
+        "items": [
+            {
+                "name": "Home",
+                "file": "Home"
+            },
+            {
+                "name": "Pages",
+                "file": "Pages"
+            },
+            {
+                "name": "History",
+                "file": "History"
+            }
+        ]
+    }
+];
