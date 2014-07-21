1bfe4c7afea33f54f761f2b0372b5a60dfec9f85
Elliot Smith <elliot.smith@intel.com>
2014-07-21 13:42:36 +0100

    [embedding API] Break links into version 1 and version 2 sections

--- documentation/APIs/20-Embedding_API.php
+++ documentation/APIs/20-Embedding_API.php
@@ -3,23 +3,28 @@ $base_url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['CONTEXT_PREFIX']. '/
 
 $docs_url_v1 = $base_url . 'embeddingapidocs/reference/org/xwalk/core/package-summary.html';
 $docs_url_v2 = $base_url . 'embeddingapidocs_v2/reference/org/xwalk/core/package-summary.html';
-$sample_url = $base_url . 'reference/org/xwalk/core/XWalkView.html';
+$sample_url_v1 = $base_url . 'embeddingapidocs/reference/org/xwalk/core/XWalkView.html';
+$sample_url_v2 = $base_url . 'embeddingapidocs_v2/reference/org/xwalk/core/XWalkView.html';
 ?>
 <h1>Embedding API</h1>
 
 <p>The embedding API enables Crosswalk to be used as a web view for Android applications, as an alternative to the stock <a href="http://developer.android.com/guide/webapps/webview.html" target="_blank">WebView</a>. You can use it to load HTML pages or whole web applications inside a Java application running on Android.</p>
 
-<p>The following Java API docs are available:</p>
+<p>The <a href="#documentation/embedding_crosswalk">Getting started with the embedding API</a> article covers basic use of the API.</p>
+
+<p>Links for more references and resources are given below.</p>
+
+<h2>Version 2 - Crosswalk 8</h2>
 
 <ul>
-  <li><a href="<?php echo $docs_url_v1 ?>" target="_blank">Javadocs (version 1)</a></li>
-  <li><a href="<?php echo $docs_url_v2 ?>" target="_blank">Javadocs (version 2)</a></li>
+  <li><a href="<?php echo $docs_url_v2 ?>" target="_blank">Javadocs v2</a></li>
+  <li><a href="<?php echo $sample_url_v2 ?>" target="_blank">Sample code for v2</a></li>
+  <li><a href="https://github.com/crosswalk-project/crosswalk/tree/master/runtime/android/sample" target="_blank">Sample project</a></li>
 </ul>
 
-<p>The following resources about the embedding API are also available:</p>
+<h2>Version 1 - Crosswalk 6 and 7</h1>
 
 <ul>
-  <li><a href="#documentation/embedding_crosswalk">Getting started with the embedding API</a></li>
-  <li><a href="<?php echo $sample_url ?>" target="_blank">Sample code</a></li>
-  <li><a href="https://github.com/crosswalk-project/crosswalk/tree/master/runtime/android/sample" target="_blank">Sample project</a></li>
+  <li><a href="<?php echo $docs_url_v1 ?>" target="_blank">Javadocs v1</a></li>
+  <li><a href="<?php echo $sample_url_v1 ?>" target="_blank">Sample code or v1</a></li>
 </ul>
