864ab95924b721115229e39c993fd0bf56800f4d
Kenneth Ormandy <kenneth@chloi.io>
2014-10-03 16:05:48 -0700

    Removes unused files

--- _old/strings.php
+++
@@ -1,9 +0,0 @@
-<?php
-// functions for manipulating strings
-function make_name ($name) {
-    return preg_replace ('/_+/', ' ',
-           preg_replace ('/-+/', ' ',
-           preg_replace ('/^[0-9]*[-_]/', '',
-           $name)));
-}
-?>
