94ff36e098d3d59fb587bf6c5315dd8c8f65c118
Bob Spencer <bob.spencer@intel.com>
2016-04-27 14:04:59 -0600

    Fixed IE bug, added visibility check
     - IE didn't rotate correctly. Removed rotation feature for IE
     - Added check for page visibility so it doesn't rotate when not active

--- public/js/app-showcase.js
+++ public/js/app-showcase.js
@@ -1,4 +1,9 @@
 // Rotate app cube on home screen
+//
+// Note: The Crosswalk Project is a BSD-licensed project.  In this file, MIT-licensed code 
+// was used from "Andy E" on StackOverflow for detecting page visibility. See addVisibilityListener() below.
+// http://stackoverflow.com/questions/1060008/is-there-a-way-to-detect-if-a-browser-window-is-not-currently-active
+// MIT license: https://opensource.org/licenses/MIT
 
 $(document).ready(function () { 
 
@@ -11,23 +16,43 @@ $(document).ready(function () {
     var spinTime = 3000;
     var fadeTime = 1000;
     var imgDir = "/assets/apps/icons/";
+    var isPageVisible = true;  //assume when document ready, we are visible
+    var isIE = detectIEBrowser();
+
+    function detectIEBrowser() {
+	var agent = window.navigator.userAgent;
+	if (agent.indexOf ('MSIE ') > 0 ||
+	    agent.indexOf('Trident/') > 0 ||
+	    agent.indexOf('Edge/') > 0) {
+	    return true;
+	}
+	return false;
+    }
+
+    //Subscribe to page visibility changes
+    addVisibilityListener();
+
+    //Retrieve app list from server. Start display (if success)
+    initAppList();
 
     // Retrieve app list from server
     // If no data received, no apps will display
-    $.ajax ( {
-	url: '/documentation/community/apps/app-fetch.php',
-	type: 'GET',
-	success: function (data) {
-	    dbRows = JSON.parse(data);
-	    if (dbRows != null) {
-		shuffle(dbRows);
-		prepCubeFace (0);
-		rotTimer = setInterval(rotateApp, waitTime);
-	    } else {
-		console.log ("No apps returned from server.");
+    function initAppList() {
+	$.ajax ( {
+	    url: '/documentation/community/apps/app-fetch.php',
+	    type: 'GET',
+	    success: function (data) {
+		dbRows = JSON.parse(data);
+		if (dbRows != null) {
+		    shuffle(dbRows);
+		    prepCubeFace (0);
+		    rotTimer = setInterval(rotateApp, waitTime);
+		} else {
+		    console.log ("No apps returned from server.");
+		}
 	    }
-	}
-    });
+	});
+    }
 
     //fisher-yates shuffle
     function shuffle (a) {
@@ -43,7 +68,7 @@ $(document).ready(function () {
     function bordersVisible(show,faceNum) {
 	var alphaStart = (show===true?0:1);
 	var alphaEnd =   (show===true?1:0);
-	$({alpha:(alphaStart)}).animate({alpha:alphaEnd}, {
+	$({alpha:alphaStart}).animate({alpha:alphaEnd}, {
 	    duration: fadeTime,
 	    step: function() {
 		for (var i=faceNum; i<=faceNum+1; i++) {
@@ -56,18 +81,30 @@ $(document).ready(function () {
     }
 
     function rotateApp() {
-	//create div contents for next cube face
-        prepCubeFace ((faceNum+1)%4);
+	//only initiate rotation if page is visible
+	if (!dbRows || !isPageVisible) {
+	    return;  //no app rotation/change unless visible
+	}
 
-        //show border of visible faces (current and +1)
-	bordersVisible(true, faceNum);
-	//scroll next cube face into view
-        rotDeg += 90;
-        $("#showcase-cube").css("transform", "rotateX(-" + rotDeg + "deg)");
-	//turn off borders after tranform finished
-	setTimeout(bordersVisible, spinTime, false, faceNum);
-	//newly scrolled face number
-        faceNum = (faceNum+1) % 4;
+	if (!isIE) { //rotate not working right in IE
+            //show border of visible faces (current and +1)
+	    bordersVisible(true, faceNum);
+	    //create div contents for next cube face
+	    prepCubeFace ((faceNum+1)%4);
+	    //scroll next cube face into view
+            rotDeg += 90;
+            $("#showcase-cube").css("transform", "rotateX(-" + rotDeg + "deg)");
+
+	    //turn off borders after tranform finished
+	    setTimeout(bordersVisible, spinTime, false, faceNum);
+	    //erase bottom of cube, now just made hidden
+	    setTimeout(function (oldFaceNum) {$("#showcase-face" + oldFaceNum).html ("")}, spinTime, faceNum);
+
+	    //newly scrolled face number
+            faceNum = (faceNum+1) % 4;
+	} else { //rotation not working right in IE
+	    prepCubeFace (0);
+	}
     }
 
     function prepCubeFace (faceNum) {
@@ -89,13 +126,39 @@ $(document).ready(function () {
               "<span class='meta'>Downloads: " + getNumEst(app.downloads) + "</span>" + 
 	    "</div>";
 	prepDiv.html (content);
+    }
 
-	//now erase the bottom and back of cube (so they don't show during rotate)
-	for (var i=(faceNum+1); i<=(faceNum+2); i++) {
-	    var f = $("#showcase-face" + (i%4));
-	    f.css('outline-color','rgba(36,180,186,0)');
-	    f.css('background-color','rgba(255,255,255,0)');
-	    f.html ("");
+    function addVisibilityListener() {
+	var hidden = "hidden";
+	if (hidden in document) {
+	    document.addEventListener("visibilitychange", onVisibilityChange);
+	} else if ((hidden = "mozHidden") in document) {
+	    document.addEventListener("mozvisibilitychange", onVisibilityChange);
+	} else if ((hidden = "webkitHidden") in document) {
+	    document.addEventListener("webkitvisibilitychange", onVisibilityChange);
+	} else if ((hidden = "msHidden") in document) {
+	    document.addEventListener("msvisibilitychange", onVisibilityChange);
+	} else if ("onfocusin" in document) { 	// IE 9 and lower
+	    document.onfocusin = document.onfocusout = onVisibilityChange;
+	} else {  // All others
+	    window.onpageshow = window.onpagehide = window.onfocus = window.onblur = onVisibilityChange;
+	}
+
+	function onVisibilityChange (evt) {
+	    var v = "visible", h = "hidden";
+	    var evtMap = {focus:v, focusin:v, pageshow:v, blur:h, focusout:h, pagehide:h};
+	    evt = evt || window.event;
+	    if (evt.type in evtMap) {
+		isPageVisible = !(evtMap[evt.type] == "hidden");
+	    } else {
+		isPageVisible = !(this[hidden]);  //if this[hidden] is defined, page is hidden
+	    }
+	}
+
+	// set the initial state (but only if browser supports the Page Visibility API)
+	if (document[hidden] !== undefined) {
+	    onVisibilityChange({type: document[hidden] ? "blur" : "focus"});
 	}
     }
+
 });
